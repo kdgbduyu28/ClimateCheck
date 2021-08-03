2705680cb7fd2dc5484721c57ebd2723dabbbf7c
unknown <kdgbduyu28@gmail.com>
2021-08-03 22:56:40 +0800

    First and Final release

---
+++ scratch.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String task2Result = await task2();
+  task3(task2Result);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future task2() async {
+  Duration threeSeconds = Duration(seconds: 3);
+  String result;
+  await Future.delayed(threeSeconds, () {
+    String result = 'task 2 data';
+    print('Task 2 complete');
+  });
+
+  return result;
+
+}
+
+void task3(String task2Result) {
+  String result = 'task 3 data';
+  print(task2Result);
+}
